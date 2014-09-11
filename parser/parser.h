02fb6b734ba06a3b1e4d6fcb4478d467ce8830bf
jgalloni91 <jgalloni91@gmail.com>
2014-09-11 02:23:59 +0000

    ahora las funciones de parser devuelven el mismo tipo de variable que la que se le pasa por default. si en el json el campo tiene otro tipo de variable este tira un error y devuelve el valor por defeceto

--- parser/parser.h
+++ parser/parser.h
@@ -8,7 +8,6 @@
 #ifndef PARSER_H_
 #define PARSER_H_
 
-
 #include <iostream>
 #include <json/json.h>
 
@@ -16,8 +15,24 @@
 
 using namespace std;
 
-	 string get_node(string data,string param,string file,int field,char* defaultData);
-	 string get_node(char* datas,string param,string file,char* defaultData);
-	 string get_node(string data,string param,string file,char* defaultData);
+string get_node(string data, string param, string file, int field,
+		char* defaultData);
+string get_node(char* datas, string param, string file, char* defaultData);
+string get_node(string data, string param, string file, char* defaultData);
+
+int get_node(string data, string param, string file, int field,
+		int defaultData);
+int get_node(char* datas, string param, string file, int defaultData);
+int get_node(string data, string param, string file, char* defaultData);
+
+float get_node(string data, string param, string file, int field,
+		float defaultData);
+float get_node(char* datas, string param, string file, char* defaultData);
+float get_node(string data, string param, string file, char* defaultData);
+
+bool get_node(string data, string param, string file, int field,
+		bool defaultData);
+bool get_node(char* datas, string param, string file, bool defaultData);
+bool get_node(string data, string param, string file, bool defaultData);
 
 #endif /* PARSER_H_ */
