5eeca7b8e3b5dbf53fc91a0578a5c8290d9340dc
rodri.zapico@gmail.com <rodri.zapico@gmail.com@9d899e6d-ea2e-f9d8-1f83-6aee8e7f478f>
2014-09-17 07:59:55 +0000

    Subo un branch donde esta todo lo que estuve haciendo desde el ultimo commit. No lo subo al trunk para no pisarme con lo de Jorge.
    Lo nuevo: salvo los poligonos irregulares, carga y funciona 100% todo desde el archivo de config Json. Simula el mundo con Box2D y se renderean los cambios. La fisica parece bastante realista, aunque obviamente se pueden ajustar los parametros de friccion de los materiales y del aire, la 'elasticidad' de las colisiones, y demas cosas para que quede mejor. Si quieren probarlo, reemplace el Json que habia por uno que tiene un mundo con bordes, 2 plataformas, y 2 pelotas que se deslizan por las plataformas y chocan entre si. Pueden cambiar los parametros ahi para ver mas o menos como anda todo.
    
    Tambien refactorice bastantes cosas para que quede un poco mas prolijo.

---
+++ branches/Rodri-branch/vista/Escenario.cpp
@@ -0,0 +1,65 @@
+/*
+ * Escenario.cpp
+ *
+ *  Created on: 1/9/2014
+ *      Author: manuel
+ */
+
+#include "Escenario.h"
+
+Escenario::Escenario(){}
+
+Escenario::~Escenario()
+{
+	ListaDibujables::iterator it = eLista.begin();
+
+	if( eLista.size() == 1 )
+	{
+		eLista.erase(it);
+		if((*it) != NULL) {
+			delete (*it);
+			*it = NULL;
+		}
+		return;
+	}
+	while( it != eLista.end()) {
+		if((*it) != NULL) {
+			delete (*it);
+			(*it) = NULL;
+		}
+		eLista.erase(it);
+	}
+}
+
+void Escenario::dibujarEscena(SDL_Renderer* renderer)
+{
+	ListaDibujables::iterator it = eLista.begin();
+
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+	SDL_RenderClear(renderer);
+
+	// si la lista tiene 1 solo elemento, dibujo el 1ero y listo.
+	if( eLista.size() == 1)
+	{
+		(*it)->dibujar();
+		return;
+	}
+
+	// voy recorriendo la lista y dibujando cada objeto dibujable.
+	for( it = eLista.begin(); it != eLista.end(); it++)
+	{
+		(*it)->dibujar();
+	}
+
+	SDL_RenderPresent(renderer);
+}
+
+void Escenario::agregarDibujable(IDibujable* dibujable)
+{
+	eLista.push_back(dibujable);
+}
+
+void Escenario::eventoRESTART()
+{
+	printf("EVENTO RESTART: RESTART ESCENARIO\n");
+}
