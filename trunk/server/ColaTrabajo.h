a7d16fffdd59eb9f8f3f585e946c29f9f76a1cfb
luraschi8@gmail.com <luraschi8@gmail.com@9d899e6d-ea2e-f9d8-1f83-6aee8e7f478f>
2014-10-06 01:04:02 +0000

    Fix para el template de la cola de trabajo.

--- trunk/server/ColaTrabajo.h
+++ trunk/server/ColaTrabajo.h
@@ -20,12 +20,44 @@ template <typename T> class ColaTrabajo {
     pthread_cond_t   m_condv;
 
 public:
-	ColaTrabajo();
-	virtual ~ColaTrabajo();
-	void add(T item);
-	T remove();
-	int size();
+    ColaTrabajo() {
+    	pthread_mutex_init(&m_mutex, NULL);
+    	pthread_cond_init(&m_condv, NULL);
+
+    }
+
+    ~ColaTrabajo() {
+    	pthread_mutex_destroy(&m_mutex);
+    	pthread_cond_destroy(&m_condv);
+    }
+
+    void add(T item) {
+        pthread_mutex_lock(&m_mutex);
+        m_cola.push_back(item);
+        pthread_cond_signal(&m_condv);
+        pthread_mutex_unlock(&m_mutex);
+    }
+
+    T remove() {
+        pthread_mutex_lock(&m_mutex);
+        while (m_cola.size() == 0) {
+            pthread_cond_wait(&m_condv, &m_mutex);
+        }
+        T item = m_cola.front();
+        m_cola.pop_front();
+        pthread_mutex_unlock(&m_mutex);
+        return item;
+    }
+
+    int size() {
+        pthread_mutex_lock(&m_mutex);
+        int size = m_cola.size();
+        pthread_mutex_unlock(&m_mutex);
+        return size;
+    }
 
 };
+
+
 
 #endif /* COLATRABAJO_H_ */
