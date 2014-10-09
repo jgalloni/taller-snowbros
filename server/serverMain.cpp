c678a2e81d1ec278ac9157a6d749ea4ded4e735b
fedeagb <fedeagb@gmail.com>
2014-10-09 22:47:13 +0000

    Cambiada la toma de parametros por consola. Faltan hacer validaciones y demas.

--- server/serverMain.cpp
+++ server/serverMain.cpp
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
 #include <string>
 #include "../Threads/Thread.h"
 #include "ColaTrabajo.h"
@@ -17,19 +19,83 @@
 #include <list>
 #include "DispatchThread.h"
 
+int isNumber(char* string);
 
 int main(int argc, char** argv)
 {
     // Process command line arguments
-    if ( argc < 3 || argc > 4 ) {
-        printf("Uso: %s <JSON Mapa> <Puerto> [<IP Opcional>]\n", argv[0]);
-        exit(-1);
+    int opt, cflag, pflag, iflag, port;
+    opt = cflag = pflag = iflag = port = 0;
+    std::string path (" "); // path al config
+    std::string ip (""); // IP en el que escucha.
+
+    while( (opt = getopt(argc, argv, "c:p:i:")) != -1 ) {
+       	switch(opt) {
+       	case 'c':
+       		if ( argc < 5 || argc > 7 ) {
+       			printf("Uso: %s -c <JSON Mapa> -p <Puerto> -i [<IP Opcional>]\n", argv[0]);
+       			exit(-1);
+       		}
+
+       		cflag = 1;
+       		path = std::string(optarg);
+       		break;
+       	case 'p':
+       		if ( argc < 3 || argc > 7 ) {
+       	        printf("Uso: %s -c <JSON Mapa> -p <Puerto> -i [<IP Opcional>]\n", argv[0]);
+       	        exit(-1);
+       	    }
+       		if(!isNumber(optarg)) {
+       			printf("El puerto %s especificado debe ser un numero\n", optarg);
+       			exit(-1);
+       		}
+       		pflag = 1;
+       		port = atoi(optarg); // Puerto en el que escucha a las conexiones.
+       		break;
+       	case 'i':
+       		if ( argc < 5 || argc > 7 ) {
+       	        printf("Uso: %s -c <JSON Mapa> -p <Puerto> -i [<IP Opcional>]\n", argv[0]);
+       	        exit(-1);
+       	    }
+       		iflag = 1;
+       		ip = std::string(optarg);
+       		break;
+       	case '?':
+       		if(optopt == 'c') {
+       			printf("Al parametro -c le falta el path\n");
+       		}
+       		else if(optopt == 'p') {
+       			printf("Al parametro -p le falta especificar el puerto\n");
+       		}
+       		else if(optopt == 'i') {
+       			printf("Al parametro -i le faltaria especificar una ip, si no quiere especificar una, no utilice el argumento -i\n");
+       		}
+       		else {
+       			printf("Parametro -%c no reconocido\n", optopt);
+       		}
+       		exit(-1);
+       	default:
+       		printf("Error no esperado en el procesamiento de comandos\n");
+       		exit(-1);
+       	}
     }
 
-    int port = atoi(argv[2]); //Puerto en el que escucha alas conexiones.
-    string ip; //IP en el que escucha.
-    if (argc == 4) {
-        ip = argv[3];
+    if (!cflag) {
+     	printf("No se especifico archivo de configuracion\n");
+       	// TODO: Verificar que este el archivo default
+       	// Seteando archivo default
+       	path = "default.conf";
+    } else {
+       // TODO: Verificar validez de path a config
+    }
+
+    if (!pflag) {
+       printf("No se especifico puerto con el comando -p\n");
+       return -1;
+    }
+
+    if (iflag) {
+    	// TODO: Verificar formato ip valida
     }
 
     //TODO: Cargar el json y el world.
@@ -90,5 +156,20 @@ int main(int argc, char** argv)
 
     // Should never get here
     exit(0);
+}
+
+int isNumber(char* string) {
+	std::string var;
+	if(!string) {
+		return 0;
+	} else {
+		var  = string;
+	}
+	for(unsigned int i = 0; i < var.length(); i++) {
+		if(!isdigit(var[i])) {
+			return 0;
+		}
+	}
+	return 1;
 }
 
