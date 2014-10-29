536fd5e2d024454a5680e0032a7fd4573346cf8a
rodri.zapico <rodri.zapico@gmail.com>
2014-10-29 02:46:12 +0000

    (cliente)  interfaz grafica implementada 100% usando openGL.

--- programaCliente/vista/Window.cpp
+++ programaCliente/vista/Window.cpp
@@ -11,11 +11,9 @@ Window::Window() {
 	window = NULL;
 	//wEscenario = NULL;
 	error = false;
-	wRenderer = NULL;
 	SCREEN_WIDTH = 480;
 	SCREEN_HEIGHT = 640;
 	wRatio = hRatio = 1;
-	background = NULL;
 }
 
 bool Window::init(int width, int height, std::string BGpath){
@@ -32,22 +30,12 @@ bool Window::init(int width, int height, std::string BGpath){
 			 return !error;
 		}
 		else{
-			wRenderer = crearRenderer(window);
-			if( !wRenderer ) {
-				error = true;
-				return !error;
-			}
-
 			ctx = initGL(window);
 
 			if( TTF_Init() < 0){
 				printf("error al abrir ttf\n");
 				return !error;
 			}
-
-			background = new Fondo(width, height);
-			background->setRenderer(wRenderer);
-			background->cargarImagen(BGpath);
 		}
 	}
 
@@ -55,33 +43,20 @@ bool Window::init(int width, int height, std::string BGpath){
 }
 
 bool Window::updateWindow(std::list<WorldItem*> & itemList, float escala){
-	Logger& log = * Logger::Instancia();
-	if(SDL_SetRenderDrawColor(wRenderer, 0x00, 0x00, 0x00, 0x00) != 0) {
-		log.log(WINDOWLOG,ERROR,"Error al settear color de dibujo de renderer 'RenderDrawColor'");
-		return false;
-	}
-
-	if(SDL_RenderClear(wRenderer) != 0 ) {
-		log.log(WINDOWLOG,ERROR,"Error al querer limpiar el Render 'RenderClear'");
-		return false;
-	}
 
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
-	//background->render();
 
 	// Dibuja uno por uno los elementos en la lista.
 	while(!itemList.empty()){
 		WorldItem * item = itemList.front();
 		itemList.pop_front();
-		ItemRenderer::render(wRenderer, item, escala);
+		ItemRenderer::render(item, escala);
+		delete item;
 		delete item;
 		item = NULL;
 	}
 
 	SDL_GL_SwapWindow(window);
-
-	SDL_RenderPresent(wRenderer);
 
 	return !error;
 }
@@ -91,58 +66,37 @@ bool Window::validarAnchoYAlto(int width, int height)
 {
 	Logger& log = * Logger::Instancia();
 
-	if (height > 0 && width > 0)
-	{
+	if (height > 0 && width > 0){
 		SCREEN_WIDTH = width;
 		SCREEN_HEIGHT = height;
-	}
-	else
-	{
-		if (!log.abrirLog(WINDOWLOG))
-		{
-			std::cout << "Error al abrir archivo de log " << WINDOWLOG << std::endl;
-			return false;
-		}
-
-		log.escribirLog(ERROR, "Altura y/o ancho menor a 0");
-		log.cerrarLog();
+	} else {
+		log.log(WINDOWLOG,ERROR, "Altura y/o ancho menor a 0");
 		error = true;
-		return !error;
 	}
 	return !error;
 }
 
-bool Window::iniciarSDL()
-{
+bool Window::iniciarSDL() {
 	Logger& log = * Logger::Instancia();
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-	{
-		if (!log.abrirLog(WINDOWLOG)) {
-			std::cout << "Error al abrir archivo de log " << WINDOWLOG << std::endl;
-			return false;
-		}
-		std::string buf ("No se pudo inicializar SDL!");
-		buf = buf + SDL_GetError();
-		log.escribirLog(ERROR, buf);
-		log.cerrarLog();
-		error = true;
-	}else{
-		//Inicio la carga de imagenes
-		int imgFlags = IMG_INIT_PNG;
-		if( !( IMG_Init( imgFlags ) & imgFlags ) ) {
-			if (!log.abrirLog(WINDOWLOG)) {
-				std::cout << "Error al abrir archivo de log " << WINDOWLOG << std::endl;
-				return false;
-			}
-			std::string buf ("SDL_image no pudo iniciar! SDL_image Error: ");
-			buf = buf + IMG_GetError();
-			log.escribirLog(ERROR, buf);
-			log.cerrarLog();
-			error = true;
-		}
+	// Inicio SDL.
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::string buff ("No se pudo inicializar SDL! ");
+		buff = buff + SDL_GetError();
+		log.log(WINDOWLOG,ERROR, buff);
+		return false;
 	}
-	return !error;
+
+	// Inicio la carga de imagenes.
+	int imgFlags = IMG_INIT_PNG;
+	if( !( IMG_Init( imgFlags ) & imgFlags ) ) {
+		std::string buff ("SDL_image no pudo iniciar! SDL_image Error: ");
+		buff = buff + IMG_GetError();
+		log.log(WINDOWLOG,ERROR, buff);
+		return false;
+	}
+
+	return true;
 }
 
 SDL_Window* Window::crearVentana()
@@ -165,43 +119,21 @@ SDL_Window* Window::crearVentana()
 	return w;
 }
 
-SDL_Renderer* Window::crearRenderer(SDL_Window* w)
-{
-	Logger& log = * Logger::Instancia();
-	SDL_Renderer* renderer = NULL;
-	renderer = SDL_CreateRenderer( w, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-	if( !renderer )
-	{
-		if (!log.abrirLog(WINDOWLOG)) {
-			std::cout << "Error al abrir archivo de log " << WINDOWLOG << std::endl;
-			return renderer;
-		}
-		log.escribirLog(ERROR, "No se pudo crear el renderer!");
-		log.cerrarLog();
-		SDL_DestroyWindow(window);
-		return renderer;
-	}
-	else SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
-	return renderer;
-}
-/*
-void Window::insertarFigura(IDibujable* figura){
-	figura->setRenderer(wRenderer);
-}*/
 
 Window::~Window(){
 
-	std::cout << "destruyendo ventana" << std::endl;
-	if(wRenderer) {
-		SDL_DestroyRenderer(wRenderer);
-		wRenderer = NULL;
-	}
-	if(ctx)
+	if (TTF_WasInit()) TTF_Quit();
+
+	if (ctx) {
 		SDL_GL_DeleteContext(ctx);
+		ctx = NULL;
+	}
+
 	if(window) {
 		SDL_DestroyWindow(window);
 		window = NULL;
 	}
+
 	SDL_Quit();
 }
 
