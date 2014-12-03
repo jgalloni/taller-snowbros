cbd8c98bd3cba1c62e0c09f5d40feebab2c61800
rodri.zapico@gmail.com <rodri.zapico@gmail.com@9d899e6d-ea2e-f9d8-1f83-6aee8e7f478f>
2014-12-03 08:58:57 +0000

    cosas

--- trunk/programaCliente/mainCliente.cpp
+++ trunk/programaCliente/mainCliente.cpp
@@ -18,6 +18,131 @@ bool isValidPort(int puerto);
 
 Window * window;
 std::list<WorldItem*> itemList;
+TCPStream * stream;
+
+bool esperarPorMasJugadores(){
+
+	ssize_t len;
+	std::string inMessage;
+	SDL_Event event;
+
+	// Espera a que el server arranque la simulacion, mostrando una pantalla de espera.
+	while(inMessage != "SERVER-LISTO") {
+
+		std::cout << "estoy esperando... recibi: " << inMessage << std::endl;
+
+		len = stream->receive(inMessage);
+		if (len <= 0) {
+			std::cout << "conexion perdida con el servidor" << std::endl;
+			return false;
+		}
+
+		while( SDL_PollEvent( &event ) != 0 ) {
+			switch(event.type){
+			case SDL_QUIT:
+				return false;
+			case SDL_KEYDOWN:
+				if (event.key.keysym.sym == SDLK_ESCAPE) return false;
+			}
+		}
+
+		Metadata * pantallaIntroFondo = new Metadata();;
+		Metadata * pantallaIntroHUD = new Metadata();;
+		pantallaIntroFondo->posXCamara = 0;
+		pantallaIntroFondo->posYCamara = 0;
+		pantallaIntroFondo->anchoCamara = 1;
+		pantallaIntroFondo->altoCamara = 1;
+		pantallaIntroFondo->tamanioXMundo = 720;
+		pantallaIntroFondo->tamanioYMundo = 640;
+		pantallaIntroFondo->tipo = METADATAFONDO;
+		pantallaIntroHUD->vidas[0] = pantallaIntroHUD->vidas[1] =
+				pantallaIntroHUD->vidas[2] = pantallaIntroHUD->vidas[3] = 0;
+		pantallaIntroHUD->users[0] = "offline"; //username;
+		pantallaIntroHUD->users[1] = pantallaIntroHUD->users[2] = pantallaIntroHUD->users[3] = "offline";
+		pantallaIntroHUD->puntaje = 0;
+		pantallaIntroHUD->mensaje.clear();
+		pantallaIntroHUD->mensaje = "NOMESSAGE-Esperando-mas-conexiones...";
+
+		itemList.push_back(pantallaIntroFondo);
+		itemList.push_back(pantallaIntroHUD);
+
+		window->updateWindow(itemList,1.0f);
+	}
+
+	return true;
+}
+
+bool protocoloFinDePartida(resultado_t resultado){
+
+	SDL_Event event;
+	bool quit = false;
+	bool continuePlaying = false;
+	std::string outMessage;
+
+	// Espera a que el jugador de una respuesta.
+	while(!quit) {
+
+		while( SDL_PollEvent( &event ) != 0 ) {
+			switch(event.type){
+			case SDL_QUIT:
+				outMessage = "STOPPLAYING";
+				stream->send(outMessage);
+				quit = true;
+				break;
+			case SDL_KEYDOWN:
+				if (event.key.keysym.sym == SDLK_ESCAPE) {
+					outMessage = "STOPPLAYING";
+					stream->send(outMessage);
+					quit = true;
+				}
+				if (event.key.keysym.sym == SDLK_y) {
+					outMessage = "CONTINUEPLAYING";
+					stream->send(outMessage);
+					continuePlaying = true;
+					quit = true;
+				}
+				if (event.key.keysym.sym == SDLK_n) {
+					outMessage = "STOPPLAYING";
+					stream->send(outMessage);
+					quit = true;
+				}
+			}
+		}
+
+		stream->send("DONE");
+
+		Metadata * pantallaIntroFondo = new Metadata();;
+		Metadata * pantallaIntroHUD = new Metadata();;
+		pantallaIntroFondo->posXCamara = 0;
+		pantallaIntroFondo->posYCamara = 0;
+		pantallaIntroFondo->anchoCamara = 1;
+		pantallaIntroFondo->altoCamara = 1;
+		pantallaIntroFondo->tamanioXMundo = 720;
+		pantallaIntroFondo->tamanioYMundo = 640;
+		pantallaIntroFondo->tipo = METADATAFONDO;
+		pantallaIntroHUD->vidas[0] = pantallaIntroHUD->vidas[1] =
+				pantallaIntroHUD->vidas[2] = pantallaIntroHUD->vidas[3] = 0;
+		pantallaIntroHUD->users[0] = "offline"; //username;
+		pantallaIntroHUD->users[1] = pantallaIntroHUD->users[2] = pantallaIntroHUD->users[3] = "offline";
+		pantallaIntroHUD->puntaje = 0;
+		pantallaIntroHUD->mensaje.clear();
+		if (resultado == GANARON) pantallaIntroHUD->mensaje = "NOMESSAGE-Ganaron!&Deseas-jugar-de-nuevo?-Y/N";
+		else pantallaIntroHUD->mensaje = "NOMESSAGE-Han sido derrotados.&Deseas-jugar-de-nuevo?-Y/N";
+
+		itemList.push_back(pantallaIntroFondo);
+		itemList.push_back(pantallaIntroHUD);
+
+		window->updateWindow(itemList,1.0f);
+	}
+
+	if(continuePlaying == true) {
+		std::cout << "voy a esperar al resto" << std::endl;
+		return esperarPorMasJugadores();
+	}
+
+	std::cout << "me cago en todo." << std::endl;
+	return false;
+}
 
 int main(int argc, char * argv[]){
 
@@ -104,7 +229,7 @@ int main(int argc, char * argv[]){
 
 	// Conecto al servidor.
     TCPConnector * connector = new TCPConnector();
-    TCPStream * stream =  connector->connect(ip.c_str(), port);
+    stream =  connector->connect(ip.c_str(), port);
     if (!stream){
     	std::cout << "Hubo un problema al conectar al servidor" << std::endl;
     	return -1;
@@ -141,46 +266,7 @@ int main(int argc, char * argv[]){
 	std::string outMessage;
 
 	// Espera a que el server arranque la simulacion, mostrando una pantalla de espera.
-	while(inMessage != "SERVER-LISTO" && !quit) {
-		len = stream->receive(inMessage);
-		if (len <= 0) {
-			quit = true;
-			std::cout << "conexion perdida con el servidor" << std::endl;
-			break;
-		}
-
-		while( SDL_PollEvent( &event ) != 0 ) {
-			switch(event.type){
-			case SDL_QUIT:
-				quit = true;
-				break;
-			case SDL_KEYDOWN:
-				if (event.key.keysym.sym == SDLK_ESCAPE) quit = true;
-			}
-		}
-
-		Metadata * pantallaIntroFondo = new Metadata();;
-		Metadata * pantallaIntroHUD = new Metadata();;
-		pantallaIntroFondo->posXCamara = 0;
-		pantallaIntroFondo->posYCamara = 0;
-		pantallaIntroFondo->anchoCamara = 1;
-		pantallaIntroFondo->altoCamara = 1;
-		pantallaIntroFondo->tamanioXMundo = 720;
-		pantallaIntroFondo->tamanioYMundo = 640;
-		pantallaIntroFondo->tipo = METADATAFONDO;
-		pantallaIntroHUD->vidas[0] = pantallaIntroHUD->vidas[1] =
-				pantallaIntroHUD->vidas[2] = pantallaIntroHUD->vidas[3] = 0;
-		pantallaIntroHUD->users[0] = username;
-		pantallaIntroHUD->users[1] = pantallaIntroHUD->users[2] = pantallaIntroHUD->users[3] = "offline";
-		pantallaIntroHUD->puntaje = 0;
-		pantallaIntroHUD->mensaje.clear();
-		pantallaIntroHUD->mensaje = "NOMESSAGE-Esperando-mas-conexiones...";
-
-		itemList.push_back(pantallaIntroFondo);
-		itemList.push_back(pantallaIntroHUD);
-
-		window->updateWindow(itemList,1.0f);
-	}
+	quit = !esperarPorMasJugadores();
 
 	while( !quit ){
 
@@ -241,9 +327,19 @@ int main(int argc, char * argv[]){
 		// no se cargo el PJ en el servidor.
 		if (inMessage == "EMPTY") continue;
 
+		std::cout << "voy a separar los elementos recibidos." << std::endl;
+
 		// Separa todos los elementos para deserializarlos uno por uno.
 		std::vector<std::string> buff;
 		split(buff, inMessage, "%", no_empties);
+
+		// Si recibe "GAMEENDED" indica que se tiene que iniciar el protocolo de fin de partida.
+		if(buff[0] == "GAMEENDED"){
+			quit = !protocoloFinDePartida((resultado_t) strtol(buff[1].c_str(),NULL,10));
+			continue;
+		}
+
+		std::cout << "deserializando la escala del mundo." << std::endl;
 
 		// El primer elemento es metadata.
 		WorldItem * item = Deserializador::deserializar(buff[0]);
@@ -252,12 +348,14 @@ int main(int argc, char * argv[]){
 		delete item;
 		item = NULL;
 
+		std::cout << "iterando sobre todas las cosas del mundo a deserializar." << std::endl;
+
 		// Itera sobre todos los elementos restantes.
 		for (std::vector<std::string>::iterator it = buff.begin(); it != buff.end(); it++){
 			item = Deserializador::deserializar((*it));
 			if (!item) continue;
 			itemList.push_back(item);
-			if(item->tipo==METADATAHUD){
+			/*if(item->tipo==METADATAHUD){
 				for(int i=0;i<4;i++){
 					if(((Metadata*)item)->users[i].compare(username)==0){
 						if(((Metadata*)item)->vidas[i]==0){
@@ -267,7 +365,7 @@ int main(int argc, char * argv[]){
 						}
 					}
 				}
-			}
+			}*/
 		}
 
 		window->updateWindow(itemList, escala);
